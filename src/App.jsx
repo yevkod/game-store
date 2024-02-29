767d0055bd1c1933267c62e31bd3c9bb93e595c6
yevgenkod <yevgen.nevidim@cowchain.io>
2024-03-01 00:49:31 +0200

    feat: add skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -11,30 +11,40 @@ import { DetailsView } from './pages/details-page/DetailsView';
 import { OrderView } from './pages/order-page/OrderView';
 import { randomOrderNumber } from './components/helpers/utils';
 import BankCard from './components/BankCard/BankCard';
+import { SkeletonTheme } from 'react-loading-skeleton';
+import { SkeletonColorClasses } from './constants/constants';
 
 
 export const App = () => {
   return (
-    <Router>
-      <div className="App overflow-hidden">
-        <NavBarView />
-        <Routes>
-          <Route path='/' element={
-            <MainView />
-          }
-          />
-          <Route path=':title' element={
-            <DetailsView />
-          } />
-          <Route path={`/order/${randomOrderNumber}`} element={
-            < OrderView />
-          } />
-          <Route path={`/payment/${randomOrderNumber}`} element={
-            <BankCard />
-          } />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <SkeletonTheme
+        baseColor={SkeletonColorClasses.baseColor}
+        highlightColor={SkeletonColorClasses.highlightColor}
+        enableAnimation
+      >
+      <Router>
+        <div className="App overflow-hidden">
+          <NavBarView />
+          <Routes>
+            <Route path='/' element={
+              <MainView />
+            }
+            />
+            <Route path=':title' element={
+              <DetailsView />
+            } />
+            <Route path={`/order/${randomOrderNumber}`} element={
+              < OrderView />
+            } />
+            <Route path={`/payment/${randomOrderNumber}`} element={
+              <BankCard />
+            } />
+          </Routes>
+        </div>
+      </Router>
+      </SkeletonTheme>
+    </>
   );
 }
 
---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+export const SkeletonColorClasses = {
+    baseColor: 'rgba(255,255,255,.05)',
+    highlightColor: 'rgba(255,255,255,.2)',
+    mb: '6px',
+  };
